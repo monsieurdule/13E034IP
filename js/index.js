38717d6b77ede66de935f144f2fb287d9995bfde
Dušan <monsieurdule@gmail.com>
2021-06-13 20:11:16 +0200

    dev

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
-var modal = document.getElementById("loginModal");
-var btn = document.getElementById("loginBtn");
+var modal;
 
-btn.onclick = function() {
+function klikDugme() {
     loginModal.style.display = "block";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+function start() {
+
+    document.getElementById("loginBtn").addEventListener("click", klikDugme);
+    modal = document.getElementById("loginModal");
+}
+
+window.onload = start;
--- js/inputdate.js
+++ js/inputdate.js
@@ -4,11 +4,19 @@ function generateForm() {
     document.getElementById("form-content").innerHTML = "";
 
     for (var i = 0; i < cnt; i++) {
-
-        document.getElementById("form-content").innerHTML += "<label for=\"date\">Radni dan:</label><br><input type=\"date\" id=\"birthday " +
+        document.getElementById("form-content").innerHTML
+            += "<label for=\"date\">Radni dan " + (i + 1) + ". praznika:</label><br><input type=\"date\" id=\"birthday " +
             cnt + " \" name=\"birthday\"><br><label for=\"nhours\">Broj sati:</label><br><input type=\"text\" id=\"nhours " +
-            cnt + "\" name=\"nhours\"><br><br><input type=\"submit\" id=\"submit" + cnt + "\">"
-
+            cnt + "\" name=\"nhours\"><br><br>"
     }
+    document.getElementById("form-content").innerHTML += "<button type=\"submit\" onclick=\"submitForm()\"> Submit </button>";
+
+}
+
+function submitForm() {
+
+    //dohvatanje php-a i  ubacivanje u bazu
+
+    window.location.href = "file:///C:/wamp64/www/13E034IP/html/medic.html";//redirect na drugu stranicu
 
 }
