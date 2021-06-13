9db2f17d2f5f96b0728d815a6ae1907b07ae7285
Dusan Jovanovic <monsieurdule@gmail.com>
2021-06-13 17:02:19 +0200

    final

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
 var modal = document.getElementById("loginModal");
-
 var btn = document.getElementById("loginBtn");
 
-btn.onclick = function () {
-    LoginModal.style.display = "block";
-    //modal.style.display = "block";
+btn.onclick = function() {
+    loginModal.style.display = "block";
 }
 
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
-//var closebtn = document.getElementById("")
